0fd364c4d88947288f0b41214910fe0a8557bd4e
nandanr <canulovemenow9164@gmail.com>
2026-03-12 10:09:19 +0530

    Fix formatting and add newline at end of hlo.java

--- hlo.java
+++ hlo.java
@@ -1,4 +1,5 @@
-public class hlo{
-public static void main(String[] args){
-  System.out.println("Hiiiiiii");
+public class hlo {
+    public static void main(String[] args) {
+        System.out.println("Hiiiiiii");
+    }
 }
